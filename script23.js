ca66d8933ad6ad930ee8477729d7d20bdb2f006f
Antoine HAMOUI <hamoui.antoine@gmail.com>
2016-05-04 15:23:35 +0200

    semiFinalSolution

--- script23.js
+++ script23.js
@@ -1,21 +1,31 @@
-
 (function () {
  var score = 0;
-   var btn = document.getElementById("btn");
-   var btn2 = document.getElementById("btn2");
+ var speed = 1000;
+ var btn = document.getElementById("btn");
+ var btn2 = document.getElementById("btn2");
+ 
    onload = function () {
-      btn.onclick = function () {
-             score = score + 1;
-       document.getElementById("display").innerHTML = score;
-         
-   };  
-   };
-   
+        btn.onclick = function () {
+        score++;
+        document.getElementById("display").innerHTML = score;
+            }
+        };  
+  
    btn2.onclick = function () {
        score = score * 2;
        document.getElementById("display").innerHTML = score;
-   };
+       };
+  
+   btn3.onclick = function () {
+    // score = 0;
+    alert("R U sure??");
+    autoadd = function (x) {
+    score = score + x;
+    document.getElementById("display").innerHTML = score;
+        }; 
+    };    
    
- 
-
+var timer = setInterval(function(){autoadd(5);}, speed);
 }());
+
+
